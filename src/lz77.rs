416b7a497be382f90e8665e522895c9800d03d18
Rift <ethanstm8@gmail.com>
2025-06-08 01:18:26 -0400

    bug fixes

--- src/lz77.rs
+++ src/lz77.rs
@@ -1,6 +1,7 @@
 
 use std::collections::HashMap;
 use std::cmp::min;
+use std::fmt::Error;
 //try using a trie to see if there's a speedup
 //use qp_trie::Trie;
 
@@ -13,19 +14,21 @@ struct Match {
     next_symbol: u8,
 }
 
+//TODO - support wrap around
 pub fn lz77_encode(source: &[u8]) -> Vec<u8> {
     let mut search_buf: &[u8] = &[];
     let mut lookahead_buf = &source[0..min(WINDOW_LEN, source.len())];
 
     let mut output = Vec::new();
 
+    //starting index of lookahead buffer
     let mut cursor: usize = 0;
+    let mut lb_iter = lookahead_buf.iter().peekable();
 
     while lookahead_buf.len() > 0 {
+        let mut best_match = Match::with_symbol(source[cursor]);
 
-        for lb_symbol in lookahead_buf {
-            let mut best_match = Match::with_symbol(source[0]);
-
+        while lb_iter.peek().is_some() {
             let mut match_len = 0;
 
             //iterate over the search buffer, and find the longest match
@@ -33,35 +36,67 @@ pub fn lz77_encode(source: &[u8]) -> Vec<u8> {
             for (sb_pos, sb_symbol) in search_buf.iter().enumerate() {
 
                 //if symbols match increment match counter
-                if sb_symbol == lb_symbol { match_len += 1; } else {
+                if sb_symbol == lb_iter.next().unwrap() { match_len += 1; } else {
 
                     //check if our current match is the longest one, and
                     //update it as our best match if so
                     if match_len > best_match.len {
                         best_match.len = match_len;
                         best_match.next_symbol = lookahead_buf[match_len as usize];
-                        best_match.offset = (search_buf.len() - sb_pos) as u8;
+                        best_match.offset = (cursor - sb_pos + 1) as u8;
                     }
 
                     match_len = 0;
                 }
             }
-
-            //output best match as a (length:offset:next_symbol) tuple
-            output.push(best_match.len);
-            output.push(best_match.offset);
-            output.push(best_match.next_symbol);
         }
 
-        cursor += 1;
+        //output best match as a (length:offset:next_symbol) tuple
+        output.push(best_match.len);
+        output.push(best_match.offset);
+        output.push(best_match.next_symbol);
+
+        //advance by the length of the match we found
+        cursor += best_match.len as usize + 1;
         //expand search buffer until it reaches size WINDOW_LEN, then shift to right
         search_buf = &source[cursor.saturating_sub(WINDOW_LEN)..cursor];
         //vice versa
         lookahead_buf = &source[cursor..min(WINDOW_LEN+cursor, source.len())];
+        //update iterator to lookahead buffer
+        lb_iter = lookahead_buf.iter().peekable();
     }
 
     output
 }
+
+/*
+pub fn lz77_decode(source: &[u8]) -> Result<Vec<u8>, Error> {
+    let mut output = Vec::new();
+
+    let mut cursor: usize = 0;
+    let mut iter = source.iter();
+
+    //extract length, offset and next symbol
+    while let Some(&len) = iter.next() {
+        let len = len as usize;
+        let offset = *iter.next().ok_or(Error)? as usize;
+        let next_symbol = *iter.next().ok_or(Error)? as usize;
+
+
+        let start = cursor - offset;
+        let mut i = 0;
+        for _ in start..start+len {
+            output.push(source[i]);
+            i += 1;
+
+            //wraparound
+            if i == cursor { i = 0; }
+        }
+    }
+
+    Ok(output)
+}
+ */
 
 
 impl Match {
